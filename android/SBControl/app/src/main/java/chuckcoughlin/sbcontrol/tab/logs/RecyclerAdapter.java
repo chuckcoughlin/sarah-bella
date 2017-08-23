1ef1a965bbeeda350745ace0c1d3fb7e20db910b
Chuck Coughlin <ccoughlin@ils-automation.com>
2017-08-22 18:56:28 -0600

    Progress with recycler adapter.

---
+++ android/SBControl/app/src/main/java/chuckcoughlin/sbcontrol/tab/logs/RecyclerAdapter.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright 2017 Charles Coughlin. All rights reserved.
+ *  (MIT License)
+ */
+package chuckcoughlin.sbcontrol.tab.logs;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * This a link between a RecyclerView and the data backstop.
+ */
+
+public class RecyclerAdapter extends RecyclerView.Adapter<LogViewHolder> {
+
+
+    @Override
+    public LogViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(LogViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
