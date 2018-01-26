328e8b04d2c8aa09d01066358d2fe4be545575a1
Chuck Coughlin <chuck_coughlin@comcast.net>
2018-01-26 09:20:36 -0700

    system_check now publishing messages

--- robot/src/system_check/src/publish_system.py
+++ robot/src/system_check/src/publish_system.py
@@ -11,9 +11,9 @@ import socket
 from system_check.msg import System
 from std_msgs.msg import String
 
-pub = rospy.Publisher('/sb_system',String,queue_size=1)
+pub = rospy.Publisher('/sb_system',System,queue_size=1)
 rospy.init_node('sb_publish_system')
-rate= rospy.Rate(10) #  10 second publish rate
+rate= rospy.Rate(0.10) #  10 second publish rate
 msg= System('hostname', 'ip_address', 'cpu_percent', 'memory_percent_used', 'free_memory_bytes', 'swap_memory_percent_used', \
             'disk_percent_used', 'packets_sent', 'packets_received', 'in_packets_dropped', 'out_packets_dropped')
 
@@ -31,20 +31,7 @@ while not rospy.is_shutdown():
 	msg.packets_received = net[3]
 	msg.in_packets_dropped = net[6]
 	msg.out_packets_dropped = net[7]
-	# All args and in-order
-	lst = []
-	lst.append(msg.hostname)
-	lst.append(msg.ip_address)
-	lst.append(msg.cpu_percent)
-	lst.append(msg.memory_percent_used)
-	lst.append(msg.free_memory_bytes)
-	lst.append(msg.swap_memory_percent_used)
-	lst.append(msg.disk_percent_used)
-	lst.append(msg.packets_sent)
-	lst.append(msg.packets_received)
-	lst.append(msg.in_packets_dropped)
-	lst.append(msg.out_packets_dropped)
-	pub.publish(lst)
+	pub.publish(msg)
 	rate.sleep()
 
 print "system_check.publish_system: complete"
