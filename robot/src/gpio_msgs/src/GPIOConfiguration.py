77d6ba520c88daedf8c50a0465a6e68806449706
Chuck Coughlin <chuck_coughlin@comcast.net>
2018-02-23 14:21:16 -0700

    iGPIO messages - just one publisher. Use parameter to trigger full update.

--- robot/src/gpio_msgs/src/GPIOConfiguration.py
+++ robot/src/gpio_msgs/src/GPIOConfiguration.py
@@ -15,245 +15,247 @@ def configure():
 
 # Argument is a GPIOState.
 def initialize(state):
+	count = state.PIN_COUNT
 	pins = []
-	state.pin1 = GPIOPin()
-	state.pin1.label = "3.3V"
-	state.pin1.channel = 1
-	state.pin1.mode = "PWR"
-	pins.append(state.pin1)
+	pin1 = GPIOPin()
+	pin1.label = "3.3V"
+	pin1.channel = 1
+	pin1.mode = "PWR"
+	pins.append(pin1)
 
-	state.pin2 = GPIOPin()
-	state.pin2.label = "5"
-	state.pin2.channel = 2
-	state.pin2.mode = "PWR"
-	pins.append(state.pin2)
+	pin2 = GPIOPin()
+	pin2.label = "5"
+	pin2.channel = 2
+	pin2.mode = "PWR"
+	pins.append(pin2)
 
-	state.pin3 = GPIOPin()
-	state.pin3.label = "BCM 2"
-	state.pin3.channel = 3
-	state.pin3.mode = GPIO.gpio_function(3)
-	pins.append(state.pin3)
+	pin3 = GPIOPin()
+	pin3.label = "BCM 2"
+	pin3.channel = 3
+	pin3.mode = GPIO.gpio_function(3)
+	pins.append(pin3)
 
-	state.pin4 = GPIOPin()
-	state.pin4.label = "5V"
-	state.pin4.channel = 4
-	state.pin4.mode = "PWR"
-	pins.append(state.pin4)
+	pin4 = GPIOPin()
+	pin4.label = "5V"
+	pin4.channel = 4
+	pin4.mode = "PWR"
+	pins.append(pin4)
 
-	state.pin5 = GPIOPin()
-	state.pin5.label = "BCM 3"
-	state.pin5.channel = 5
-	state.pin5.mode = GPIO.gpio_function(5)
-	pins.append(state.pin5)
+	pin5 = GPIOPin()
+	pin5.label = "BCM 3"
+	pin5.channel = 5
+	pin5.mode = GPIO.gpio_function(5)
+	pins.append(pin5)
 
-	state.pin6 = GPIOPin()
-	state.pin6.label = "GND"
-	state.pin6.channel = 6
-	state.pin6.mode = "GND"
-	pins.append(state.pin6)
+	pin6 = GPIOPin()
+	pin6.label = "GND"
+	pin6.channel = 6
+	pin6.mode = "GND"
+	pins.append(pin6)
 
-	state.pin7 = GPIOPin()
-	state.pin7.label = "BCM 4"
-	state.pin7.channel = 7
-	state.pin7.mode = GPIO.gpio_function(7)
-	pins.append(state.pin7)
+	pin7 = GPIOPin()
+	pin7.label = "BCM 4"
+	pin7.channel = 7
+	pin7.mode = GPIO.gpio_function(7)
+	pins.append(pin7)
 
-	state.pin8 = GPIOPin()
-	state.pin8.label = "BCM 14"
-	state.pin8.channel = 8
-	state.pin8.mode = GPIO.gpio_function(8)
-	pins.append(state.pin8)
+	pin8 = GPIOPin()
+	pin8.label = "BCM 14"
+	pin8.channel = 8
+	pin8.mode = GPIO.gpio_function(8)
+	pins.append(pin8)
 
-	state.pin9 = GPIOPin()
-	state.pin9.label = "GND"
-	state.pin9.channel = 9
-	state.pin9.mode = "GND"
-	pins.append(state.pin9)
+	pin9 = GPIOPin()
+	pin9.label = "GND"
+	pin9.channel = 9
+	pin9.mode = "GND"
+	pins.append(pin9)
 
-	state.pin10 = GPIOPin()
-	state.pin10.label = "BCM 15"
-	state.pin10.channel = 10
-	state.pin10.mode = GPIO.gpio_function(10)
-	pins.append(state.pin10)
+	pin10 = GPIOPin()
+	pin10.label = "BCM 15"
+	pin10.channel = 10
+	pin10.mode = GPIO.gpio_function(10)
+	pins.append(pin10)
 
-	state.pin11 = GPIOPin()
-	state.pin11.label = "BCM 17"
-	state.pin11.channel = 11
-	state.pin11.mode = GPIO.gpio_function(11)
-	pins.append(state.pin11)
+	pin11 = GPIOPin()
+	pin11.label = "BCM 17"
+	pin11.channel = 11
+	pin11.mode = GPIO.gpio_function(11)
+	pins.append(pin11)
 
-	state.pin12 = GPIOPin()
-	state.pin12.label = "BCM 18"
-	state.pin12.channel = 12
-	state.pin12.mode = GPIO.gpio_function(12)
-	pins.append(state.pin12)
+	pin12 = GPIOPin()
+	pin12.label = "BCM 18"
+	pin12.channel = 12
+	pin12.mode = GPIO.gpio_function(12)
+	pins.append(pin12)
 
-	state.pin13 = GPIOPin()
-	state.pin13.label = "BCM 27"
-	state.pin13.mode = "PWR"
-	state.pin13.mode = GPIO.gpio_function(13)
-	pins.append(state.pin13)
+	pin13 = GPIOPin()
+	pin13.label = "BCM 27"
+	pin13.mode = "PWR"
+	pin13.mode = GPIO.gpio_function(13)
+	pins.append(pin13)
 
-	state.pin14 = GPIOPin()
-	state.pin14.label = "GND"
-	state.pin14.channel = 14
-	state.pin14.mode = "GND"
-	pins.append(state.pin14)
+	pin14 = GPIOPin()
+	pin14.label = "GND"
+	pin14.channel = 14
+	pin14.mode = "GND"
+	pins.append(pin14)
 
-	state.pin15 = GPIOPin()
-	state.pin15.label = "BCM 22"
-	state.pin15.channel = 15
-	state.pin15.mode = GPIO.gpio_function(15)
-	pins.append(state.pin15)
+	pin15 = GPIOPin()
+	pin15.label = "BCM 22"
+	pin15.channel = 15
+	pin15.mode = GPIO.gpio_function(15)
+	pins.append(pin15)
 
-	state.pin16 = GPIOPin()
-	state.pin16.label = "BCM 23"
-	state.pin16.channel = 16
-	state.pin16.mode = GPIO.gpio_function(16)
-	pins.append(state.pin16)
+	pin16 = GPIOPin()
+	pin16.label = "BCM 23"
+	pin16.channel = 16
+	pin16.mode = GPIO.gpio_function(16)
+	pins.append(pin16)
 
-	state.pin17 = GPIOPin()
-	state.pin17.label = "3.3V"
-	state.pin17.channel = 17
-	state.pin17.mode = "PWR"
-	pins.append(state.pin17)
+	pin17 = GPIOPin()
+	pin17.label = "3.3V"
+	pin17.channel = 17
+	pin17.mode = "PWR"
+	pins.append(pin17)
 
-	state.pin18 = GPIOPin()
-	state.pin18.label = "BCM 24"
-	state.pin18.channel = 18
-	state.pin18.mode = GPIO.gpio_function(18)
-	pins.append(state.pin18)
+	pin18 = GPIOPin()
+	pin18.label = "BCM 24"
+	pin18.channel = 18
+	pin18.mode = GPIO.gpio_function(18)
+	pins.append(pin18)
 
-	state.pin19 = GPIOPin()
-	state.pin19.label = "BCM 10"
-	state.pin19.channel = 19
-	state.pin19.mode = GPIO.gpio_function(19)
-	pins.append(state.pin19)
+	pin19 = GPIOPin()
+	pin19.label = "BCM 10"
+	pin19.channel = 19
+	pin19.mode = GPIO.gpio_function(19)
+	pins.append(pin19)
 
-	state.pin20 = GPIOPin()
-	state.pin20.label = "GND"
-	state.pin20.channel = 20
-	state.pin20.mode = "GND"
-	pins.append(state.pin20)
+	pin20 = GPIOPin()
+	pin20.label = "GND"
+	pin20.channel = 20
+	pin20.mode = "GND"
+	pins.append(pin20)
 
-	state.pin21 = GPIOPin()
-	state.pin21.label = "BCM 9"
-	state.pin21.channel = 21
-	state.pin21.mode = GPIO.gpio_function(21)
-	pins.append(state.pin21)
+	pin21 = GPIOPin()
+	pin21.label = "BCM 9"
+	pin21.channel = 21
+	pin21.mode = GPIO.gpio_function(21)
+	pins.append(pin21)
 
-	state.pin22 = GPIOPin()
-	state.pin22.label = "BCM 25"
-	state.pin22.channel = 22
-	state.pin22.mode = GPIO.gpio_function(22)
-	pins.append(state.pin22)
+	pin22 = GPIOPin()
+	pin22.label = "BCM 25"
+	pin22.channel = 22
+	pin22.mode = GPIO.gpio_function(22)
+	pins.append(pin22)
 
-	state.pin23 = GPIOPin()
-	state.pin23.label = "BCM 11"
-	state.pin23.channel = 23
-	state.pin23.mode = GPIO.gpio_function(23)
-	pins.append(state.pin23)
+	pin23 = GPIOPin()
+	pin23.label = "BCM 11"
+	pin23.channel = 23
+	pin23.mode = GPIO.gpio_function(23)
+	pins.append(pin23)
 
-	state.pin24 = GPIOPin()
-	state.pin24.label = "BCM 8"
-	state.pin24.channel = 24
-	state.pin24.mode = GPIO.gpio_function(24)
-	pins.append(state.pin24)
+	pin24 = GPIOPin()
+	pin24.label = "BCM 8"
+	pin24.channel = 24
+	pin24.mode = GPIO.gpio_function(24)
+	pins.append(pin24)
 
-	state.pin25 = GPIOPin()
-	state.pin25.label = "GND"
-	state.pin25.channel = 25
-	state.pin25.mode = "GND"
-	pins.append(state.pin25)
+	pin25 = GPIOPin()
+	pin25.label = "GND"
+	pin25.channel = 25
+	pin25.mode = "GND"
+	pins.append(pin25)
 
-	state.pin26 = GPIOPin()
-	state.pin26.label = "BCM 7"
-	state.pin26.channel = 26
-	state.pin26.mode = GPIO.gpio_function(26)
-	pins.append(state.pin26)
+	pin26 = GPIOPin()
+	pin26.label = "BCM 7"
+	pin26.channel = 26
+	pin26.mode = GPIO.gpio_function(26)
+	pins.append(pin26)
 
-	state.pin27 = GPIOPin()
-	state.pin27.label = "BCM 0"
-	state.pin27.channel = 27
-	state.pin27.mode = GPIO.gpio_function(27)
-	pins.append(state.pin27)
+	pin27 = GPIOPin()
+	pin27.label = "BCM 0"
+	pin27.channel = 27
+	pin27.mode = GPIO.gpio_function(27)
+	pins.append(pin27)
 
-	state.pin28 = GPIOPin()
-	state.pin28.label = "BCM 1"
-	state.pin28.channel = 28
-	state.pin28.mode = GPIO.gpio_function(28)
-	pins.append(state.pin28)
+	pin28 = GPIOPin()
+	pin28.label = "BCM 1"
+	pin28.channel = 28
+	pin28.mode = GPIO.gpio_function(28)
+	pins.append(pin28)
 
-	state.pin29 = GPIOPin()
-	state.pin29.label = "BCM 5"
-	state.pin29.channel = 29
-	state.pin29.mode = GPIO.gpio_function(29)
-	pins.append(state.pin29)
+	pin29 = GPIOPin()
+	pin29.label = "BCM 5"
+	pin29.channel = 29
+	pin29.mode = GPIO.gpio_function(29)
+	pins.append(pin29)
 
-	state.pin30 = GPIOPin()
-	state.pin30.label = "GND"
-	state.pin30.channel = 30
-	state.pin30.mode = "GND"
-	pins.append(state.pin30)
+	pin30 = GPIOPin()
+	pin30.label = "GND"
+	pin30.channel = 30
+	pin30.mode = "GND"
+	pins.append(pin30)
 
-	state.pin31 = GPIOPin()
-	state.pin31.label = "BCM 6"
-	state.pin31.channel = 31
-	state.pin31.mode = GPIO.gpio_function(31)
-	pins.append(state.pin31)
+	pin31 = GPIOPin()
+	pin31.label = "BCM 6"
+	pin31.channel = 31
+	pin31.mode = GPIO.gpio_function(31)
+	pins.append(pin31)
 
-	state.pin32 = GPIOPin()
-	state.pin32.label = "BCM 12"
-	state.pin32.channel = 32
-	state.pin32.mode = GPIO.gpio_function(32)
-	pins.append(state.pin32)
+	pin32 = GPIOPin()
+	pin32.label = "BCM 12"
+	pin32.channel = 32
+	pin32.mode = GPIO.gpio_function(32)
+	pins.append(pin32)
 
-	state.pin33 = GPIOPin()
-	state.pin33.label = "BCM 13"
-	state.pin33.channel = 33
-	state.pin33.mode = GPIO.gpio_function(33)
-	pins.append(state.pin33)
+	pin33 = GPIOPin()
+	pin33.label = "BCM 13"
+	pin33.channel = 33
+	pin33.mode = GPIO.gpio_function(33)
+	pins.append(pin33)
 
-	state.pin34 = GPIOPin()
-	state.pin34.label = "GND"
-	state.pin34.channel = 34
-	state.pin34.mode = "GND"
-	pins.append(state.pin34)
+	pin34 = GPIOPin()
+	pin34.label = "GND"
+	pin34.channel = 34
+	pin34.mode = "GND"
+	pins.append(pin34)
 
-	state.pin35 = GPIOPin()
-	state.pin35.label = "BCM 19"
-	state.pin35.channel = 35
-	state.pin35.mode = GPIO.gpio_function(35)
-	pins.append(state.pin35)
+	pin35 = GPIOPin()
+	pin35.label = "BCM 19"
+	pin35.channel = 35
+	pin35.mode = GPIO.gpio_function(35)
+	pins.append(pin35)
 
-	state.pin36 = GPIOPin()
-	state.pin36.label = "BCM 16"
-	state.pin36.channel = 36
-	state.pin36.mode = GPIO.gpio_function(36)
-	pins.append(state.pin36)
+	pin36 = GPIOPin()
+	pin36.label = "BCM 16"
+	pin36.channel = 36
+	pin36.mode = GPIO.gpio_function(36)
+	pins.append(pin36)
 
-	state.pin37 = GPIOPin()
-	state.pin37.label = "BCM 26"
-	state.pin37.channel = 37
-	state.pin37.mode = GPIO.gpio_function(37)
-	pins.append(state.pin37)
+	pin37 = GPIOPin()
+	pin37.label = "BCM 26"
+	pin37.channel = 37
+	pin37.mode = GPIO.gpio_function(37)
+	pins.append(pin37)
 
-	state.pin38 = GPIOPin()
-	state.pin38.label = "BCM 20"
-	state.pin38.channel = 38
-	state.pin38.mode = GPIO.gpio_function(38)
-	pins.append(state.pin38)
+	pin38 = GPIOPin()
+	pin38.label = "BCM 20"
+	pin38.channel = 38
+	pin38.mode = GPIO.gpio_function(38)
+	pins.append(pin38)
 
-	state.pin39 = GPIOPin()
-	state.pin39.label = "GND"
-	state.pin39.channel = 39
-	state.pin39.mode = "GND"
-	pins.append(state.pin39)
+	pin39 = GPIOPin()
+	pin39.label = "GND"
+	pin39.channel = 39
+	pin39.mode = "GND"
+	pins.append(pin39)
 
-	state.pin40= GPIOPin()
-	state.pin40.label = "BCM `21"
-	state.pin40.channel = 40
-	state.pin40.mode = GPIO.gpio_function(40)
-	pins.append(state.pin40)
-
+	pin40= GPIOPin()
+	pin40.label = "BCM `21"
+	pin40.channel = 40
+	pin40.mode = GPIO.gpio_function(40)
+	pins.append(pin40)
+	
+	state.pins = pins
 	return pins
