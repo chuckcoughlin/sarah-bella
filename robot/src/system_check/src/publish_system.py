0c38d1a7383bbaf8f0da769de0466cf532999552
Chuck Coughlin <chuck_coughlin@comcast.net>
2018-04-15 07:30:47 -0600

    Added header to System message, logging to System publisher.

--- robot/src/system_check/src/publish_system.py
+++ robot/src/system_check/src/publish_system.py
@@ -9,17 +9,22 @@ import psutil
 import rospy
 import socket
 from system_check.msg import System
+from std_msgs.msg import Header
 from std_msgs.msg import String
 
 pub = rospy.Publisher('/sb_system',System,queue_size=1)
 rospy.init_node('sb_publish_system')
 rate= rospy.Rate(0.10) #  10 second publish rate
-msg= System('hostname', 'ip_address', 'cpu_percent', 'memory_percent_used',\
+msg= System('header','hostname', 'ip_address', 'cpu_percent', 'memory_percent_used',\
             'free_memory_bytes', 'swap_memory_percent_used', \
             'disk_percent_used', 'packets_sent', 'packets_received',\
             'in_packets_dropped', 'out_packets_dropped')
 
 while not rospy.is_shutdown():
+	header = Header()
+	header.stamp = rospy.Time.now()
+	header.frame_id = '0'
+	msg.header = header
 	msg.hostname = socket.gethostname()
 	msg.ip_address = socket.gethostbyname(msg.hostname)
 	msg.cpu_percent = psutil.cpu_percent(interval=1)
@@ -34,6 +39,9 @@ while not rospy.is_shutdown():
 	msg.in_packets_dropped = net[6]
 	msg.out_packets_dropped = net[7]
 	pub.publish(msg)
+	# NOTE: publishing sets the header sequence
+	if msg.header.seq % 30 == 0:
+		rospy.loginfo("System: cpu: %f, mem: %f, packets: %d/%d" % (msg.cpu_percent,msg.free_memory_bytes,msg.packets_sent,msg.packets_received))
 	rate.sleep()
 
 rospy.loginfo("complete")
