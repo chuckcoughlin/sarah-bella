19795604b0a550f4d200ca13cb2174fd05271cd8
Chuck Coughlin <chuck_coughlin@comcast.net>
2018-09-21 14:01:56 -0600

    Multiple parking changes

--- robot/src/automatic_parking/src/automatic_parking.py
+++ robot/src/automatic_parking/src/automatic_parking.py
@@ -47,7 +47,7 @@ INFINITY    = 10.
 class Position(object):
 	def __init__(self, name):
 		self.name = name
-		self.distance = 0.0
+		self.dist = 0.0
 		self.angle = 0.0
 		self.width = 0   # ~ m
 
@@ -68,7 +68,7 @@ class Parker:
 		self.behavior = ""
 		self.leftTower = Position('left_tower')
 		self.rightTower= Position('right_tower')
-		self.towerSeparation = -1.
+		self.towerSeparation = -1
 		self.targetX = -1.
 		self.targetY = -1.
 
@@ -80,7 +80,7 @@ class Parker:
 	# Start by finding the parking spot
 	def start(self):
 		self.stopped = False
-		self.report("Parker: auto park started ...");
+		self.report("Parker: started ...");
 		# Publish movement commands to the turtlebot's base
 		self.pub = rospy.Publisher('/cmd_vel', Twist,queue_size=1)
 		self.step = 0
@@ -103,20 +103,20 @@ class Parker:
 			self.find_parking_markers(scan)
 			if self.step == 0:
 				# We stay in this state until we find the towers...
-				self.report("Park: Step 0 - searching for markers")
+				self.report("Park0: searching for markers")
 				if self.rightTower.valid and self.leftTower.valid:
 					rospy.loginfo("=================================")
 					rospy.loginfo("|        |   dist    |   angle   |")
-					rospy.loginfo('| left  | {0>:.2f}| {1:>.0f}|'.format(self.leftTower.dist,\
+					rospy.loginfo('| left  | {0:.2f}| {1:.0f}|'.format(self.leftTower.dist,\
 											np.rad2deg(self.leftTower.angle)))
-					rospy.loginfo('| right | {0>:.2f}| {1:>.0f}|'.format(self.rightTower.dist,\
+					rospy.loginfo('| right | {0:.2f}| {1:.0f}|'.format(self.rightTower.dist,\
 											np.rad2deg(self.rightTower.angle)))
 					rospy.loginfo("=================================")
 					self.step = 1
 				else:
 					self.spub.publish("ERROR: Failed to find parking spot")
-			elif step == 1:
-				self.report("Park: Step 1 - proceed to reference point")
+			elif self.step == 1:
+				self.report("Park1: proceed to reference point")
 				self.setTarget(0,START_OFFSET+ROBOT_WIDTH)
 				if self.twist.linear.x<IGNORE:
 					time.sleep(1)
@@ -125,35 +125,34 @@ class Parker:
 					self.pivot(0.0)
 					self.stop()
 					self.step = 2
-			elif step == 2:
-				self.report("Park: Step 2 - downwind leg")
+			elif self.step == 2:
+				self.report("Park2: downwind leg")
 				self.setTarget(START_OFFSET,START_OFFSET+ROBOT_WIDTH)
 				if self.twist.linear.x<IGNORE:
 					self.step = 3
-			elif step == 3:
-				self.report("Park: Step 3 - base leg")
+			elif self.step == 3:
+				self.report("Park3: base leg")
 				self.setTarget(START_OFFSET,1.5*ROBOT_WIDTH)
 				if self.twist.linear.x<IGNORE:
 					self.step = 4
-			elif step == 4:
-				self.report("Park: Step 4 - final approach")
+			elif self.step == 4:
+				self.report("Park4: final approach")
 				self.setTarget(1.5*ROBOT_WIDTH,1.5*ROBOT_WIDTH)
 				if self.twist.linear.x<IGNORE:
 					self.step = 5
-					self.report("Auto_parking complete.")
-			elif step == 5:
-				self.report("Park: Step 5 - reverse angle")
+			elif self.step == 5:
+				self.report("Park5: reverse angle")
 				self.setTarget(self.towerSeparation/2.,0.)
 				self.reverse()
 				if self.twist.linear.x<IGNORE:
 					self.step = 6
-			elif step==6:
+			elif self.step==6:
 				self.report("Auto_parking complete.")
 				time.sleep(1)
 				self.reset_pub.publish(self,reset)
 				time.sleep(3)	
 				self.pivot(0.0)
-			cmd_pub.publish(self.twist)
+			self.pub.publish(self.twist)
 
 	# =============================== End of Steps ========================
 
@@ -161,7 +160,7 @@ class Parker:
 		count = 0
 		position = Position('start')
 		position.valid = False
-		position.distance = INFINITY
+		position.dist = INFINITY
 		position.angle = 0.
 		while not position.valid and count<5:
 			position = self.find_next_position(scan,position)
@@ -170,10 +169,10 @@ class Parker:
 		
 		position = Position('start')
 		position.valid = False
-		position.distance = pos1.distance
+		position.dist = pos1.dist
 		position.angle    = pos1.angle
 		while not position.valid and count<5:
-			position = self.find_next_position(scan,pos1.distance,pos1.angle)
+			position = self.find_next_position(scan,position)
 			count = count+1
 		pos2 = position
 		if pos1.angle>pos2.angle:
@@ -182,17 +181,17 @@ class Parker:
 		else:
 			self.leftTower  = pos1
 			self.rightTower = pos2
-		self.leftTower.name = 'LeftTower'
-		self.rightTower.name= 'RightTower'
+		self.leftTower.name = 'leftTower'
+		self.rightTower.name= 'rightTower'
 		# If we've never computed distance between, do it and save it
 		# Use law of cosines
 		if self.towerSeparation<0:
-			a = pos1.distance
-			b = pos2.distance
+			a = pos1.dist
+			b = pos2.dist
 			angle = pos1.angle-pos2.angle
-			self.towerSeparation = math.abs(math.sqrt(a*a+b*b-2.*a*b*math.cos(angle))
+			self.towerSeparation = math.fabs(math.sqrt(a*a+b*b-2.*a*b*math.cos(angle)))
 			rospy.loginfo("Park: Tower separation {:.2f}".format(self.towerSeparation))
-
+			rospy.loginfo(" a,b,angle: {:.2f} {:.2f} {:.2f}".format(a,b,angle))
 
 	# Search the scan results for the next-closest pillar
 	def find_next_position(self,scan,startPosition):
@@ -201,8 +200,8 @@ class Parker:
 		angle  = scan.angle_min-delta
 		for d in scan.ranges:
 			angle = angle + delta
-			if d>ignore and angle>startPosition.angle and d<startPosition.distance:
-				position.distance = d
+			if d>IGNORE and angle>startPosition.angle and d<startPosition.dist:
+				position.dist = d
 		# Now get the angle span
 		minAngle = 0
 		maxAngle    = 2*math.pi
@@ -212,7 +211,7 @@ class Parker:
 		b = INFINITY
 		for d in scan.ranges:
 			angle = angle + delta
-			if d>=position.distance and d<position.distance+TOLERANCE:
+			if d>=position.dist and d<position.dist+TOLERANCE:
 				minAngle = angle
 				inArc = True
 			elif inArc:
@@ -231,7 +230,7 @@ class Parker:
 
 		
 
-	# Angle is with respect to x-axis (towerLeft>towerRight)
+	# Angle is with respect to x-axis (leftTower>rightTower)
 	def pivot(self,angle):
 		self.twist.linear.x  = 0.0
 		self.twist.angular.z = 0.0
@@ -239,17 +238,20 @@ class Parker:
 	# Compute the current position as a projection on the x-axis
 	# We have the LIDAR positions to the tower.
 	def projectionX(self):
-		# By law of sines, with c as angle at towerRight
-		sinc = self.towerLeft.distance*math.sin(self.towerLeft.angle-self.towerRight.angle)/\
+		# By law of sines, with c as angle at rightTower
+		sinc = self.leftTower.dist*math.sin(self.leftTower.angle-self.rightTower.angle)/\
 				    					self.towerSeparation
+		rospy.loginfo("ProjectionX: {:.2f} {:.2f} {:.2f} {:.2f}".format(\
+			sinc,self.leftTower.dist,sin(self.leftTower.angle-self.rightTower.angle),\
+			self.towerSeparation)
 		cosc = math.sqrt(1. - sinc*sinc)
-		return cosc*self.towerLeft.distance - self.towerSeparation
+		return cosc*self.leftTower.dist - self.towerSeparation
 
 	def projectionY(self):
-		# By law of sines, with c as angle at towerRight
-		sinc = self.towerLeft.distance*math.sin(self.towerLeft.angle-self.towerRight.angle)/\
+		# By law of sines, with c as angle at rightTower
+		sinc = self.leftTower.dist*math.sin(self.leftTower.angle-self.rightTower.angle)/\
 				    					self.towerSeparation
-		return sinc*self.towerLeft.distance
+		return sinc*self.leftTower.dist
 
 	# Turn toward target. Target 0->2PI.
 	def rampedAngle(self):
@@ -265,19 +267,19 @@ class Parker:
 		return -angle
 
 	def reverse(self):
-		self.twist.distance = -self.twist.distance
+		self.twist.linear.x = -self.twist.linear.x
 
 	# Specify the target destination in terms of a reference system
-	# origin at towerLeft with x-axis through towerRight.
+	# origin at leftTower with x-axis through rightTower.
 	# we start the maneuvers. We travel to this point and pivot.
 	# If the left tower is the origin and the right tower on the x-axis,
-    # then the reference point is at (0,ROBOT_WIDTH+START_OFFSET)
+	# then the reference point is at (0,ROBOT_WIDTH+START_OFFSET)
 	def setTarget(self,x,y):
 		dx = self.projectionX()
-		dy = self.projectonY()
+		dy = self.projectionY()
 		x = x + dx
 		y = y + dy
-		self.twist.angular.z = math.arctan(y/x)
+		self.twist.angular.z = math.atan2(x,y)
 		self.twist.linear.x  = math.sqrt(x*x+y*y)
 		if self.twist.linear.x<MAX_SPEED:
 			self.twist.linear.x = MAX_SPEED
